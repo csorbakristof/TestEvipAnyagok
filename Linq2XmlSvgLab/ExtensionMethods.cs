2f7c17be1bd88978c760ce7e8357ef7b4ea6d9d1
Kristof <kristof@aut.bme.hu>
2018-09-27 14:29:51 +0200

    Copied Linq2XmlSvgLab (public, empty frame)

---
+++ Linq2XmlSvgLab/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Linq2XmlSvgLab
+{
+    // Amennyiben áttekinthetőbbnek érzed a kódot, bizonyos funkciókat extension methodokba
+    // is megvalósíthatsz. Az alábbi példa után egy XElement típusú rect változóra lesz "rect.GetId();".
+    static class ExtensionMethods
+    {
+        //static public string GetId(this XElement self)
+        //{
+        //    return self.Attribute("id").Value;
+        //}
+    }
+}
---
+++ Linq2XmlSvgLab/Solutions.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Linq2XmlSvgLab
+{
+    class Solutions
+    {
+        readonly XElement root;
+        readonly XNamespace ns = "http://www.w3.org/2000/svg";
+
+        public Solutions(string svgFileName)
+        {
+            root = XElement.Load(svgFileName);
+        }
+
+        private IEnumerable<XElement> Rects => root.Descendants(ns + "rect");
+
+        #region A laborfeladatok megoldásai
+        // Minden téglalap (rect elem) felsorolása
+        internal IEnumerable<XElement> GetAllRectangles()
+        {
+            return Rects;
+        }
+
+        // Hány olyan szöveg van, aminek ez a tartalma?
+        internal int CountTextsWithValue(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Téglalap szűrések
+        // Minden olyan rect elem felsorolása, aminek a kerete adott vastagságú.
+        //  A keretvastagság (más beállításokkal együtt) a "style" szöveges attribútumban
+        //  szerepel, pl. "stroke-width:2".
+        internal IEnumerable<XElement> GetRectanglesWithStrokeWidth(int width)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Adott x koordinátájú téglalapok színének visszaadása szövegesen (pl. piros esetén "#ff0000").
+        internal IEnumerable<string> GetColorOfRectanglesWithGivenX(double x)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Az adott ID-jú téglalap pozíciójának (x,y) visszaadása.
+        internal (double x, double y) GetRectangleLocationById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // A legnagyobb y értékkel rendezkező téglalap ID-jának visszaadása.
+        internal string GetIdOfRectangeWithLargestY()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Minden olyan téglalap ID-jának felsorolása, ami legalább kétszer olyan magas mint széles.
+        internal IEnumerable<string> GetRectanglesAtLeastTwiceAsHighAsWide()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Group kezelés
+        // Adott ID-jú group-ban lévő téglalapok színét sorolja fel.
+        internal IEnumerable<string> GetColorsOfRectsInGroup(string id)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Téglalapok és szövegek viszonya
+        // Minden olyan rect elem felsorolása, amiben van bármilyen szöveg.
+        //  (Olyan rect, aminek a területén van egy szövegnek a kezdőpontja (x,y).)
+        internal IEnumerable<XElement> GetRectanglesWithTextInside()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Adott színű téglalapon belüli szöveg visszaadása.
+        //  Feltételezhetjük, hogy csak egyetlen ilyen színű téglalap van és abban egyetlen
+        //  szöveg szerepel.
+        internal string GetSingleTextInSingleRectangleWithColor(string color)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Minden téglalapon kívüli szöveg felsorolása.
+        internal IEnumerable<string> GetTextsOutsideRectangles()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Téglalapok egymáshoz képesti viszonya
+        // Az egyetlen olyan téglalap pár visszaadása (id attribútumuk értékével), amik legfeljebb
+        //  adott távolságra vannak egymástól.
+        // (Itt nem gond, ha foreach-et használsz, de jobb, ha nem.)
+        internal (string id1, string id2) GetSingleRectanglePairCloseToEachOther(double maxDistance)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region ILookup és Aggregate használata
+        // Egy ILookup visszaadása, mely minden szöveghez megadja az ilyen szöveget tartalmazó
+        //  téglalapok színét. (Az ILookup-ban csak azok a szövegek szerepelnek kulcsként, amikhez van
+        //  is téglalap.)
+        internal ILookup<string, string> GetBoundingRectangleColorListForEveryText()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Minden téglalapon belüli szöveg ABC sorrendben egymás mögé fűzése, ", "-zel elválasztva.
+        //  (Az "OrderBy(s=>s)" rendezése most elegendő lesz.)
+        // Használd az Aggregate Linq metódust egy StringBuilderrel az összegyűjtéshez!
+        internal string ConcatenateOrderedTextsInsideRectangles()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Az adott kontúrszélességű (stroke width) téglalapok által együttesen lefedett terület
+        //  szélességét és magasságát adja meg
+        internal (double w, double h) GetEffectiveWidthAndHeight(int strokeThickness)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #endregion
+
+        #region Segédmetódusok
+        // Ezeknek a metódusoknak az implementálása nem kötelező, csak ajánlás.
+        //  Ezekre a funkciókra lehet, hogy többször is szükséged lesz a feladatok
+        //  megoldása során, így érdemes őket kiszervezni külön metódusokba.
+        private bool IsAtLeastTwiceAsHighAsWide(XElement rect)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerable<XElement> GetRectanglesWithColor(string color)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsInside(XElement rect, (double x, double y) p)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool AreClose(XElement r1, XElement r2, double maxDistance)
+        {
+            throw new NotImplementedException();
+        }
+
+        private (double left,double top,double right,double bottom) GetRectBoundaries(XElement r)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
